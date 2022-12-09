0181c209afb67112060dc5ff0bc4c3d97d8e12de
AnnaBalitskaya <balitskaya.anna1@gmail.com>
2022-12-09 14:36:01 +0300

    test4

---
+++ src/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(java.lang.String[] args) {
+        int myInt;
+        myInt = 557;
+        short myShort = 3266;
+        long myLong = 24725522;
+
+        double myDouble = 235.22;
+        float myFloat = 2232.4f;
+
+        char c = 'a';
+        boolean b = true;
+        byte myByte = 100; //128-128
+        System.out.println(myInt);
+    }
+}
